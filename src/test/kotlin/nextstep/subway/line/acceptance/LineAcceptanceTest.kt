fa2570c85838a8f493da6f5516800ba2e83df850
javajigi <javajigi@gmail.com>
2020-08-10 19:26:54 +0900

    refactor: java to kotlin

--- src/test/kotlin/nextstep/subway/line/acceptance/LineAcceptanceTest.kt
+++ src/test/kotlin/nextstep/subway/line/acceptance/LineAcceptanceTest.kt
@@ -52,65 +52,44 @@ class LineAcceptanceTest : AcceptanceTest() {
         // 지하철_노선_생성_실패됨
     }
 
-    // given
-    // 지하철_노선_등록되어_있음
-    // 지하철_노선_등록되어_있음
+    @Test
+    @DisplayName("지하철 노선 목록을 조회한다.")
+    fun getLines() {
+        // given
+        // 지하철_노선_등록되어_있음
+        // 지하철_노선_등록되어_있음
 
-    // when
-    // 지하철_노선_목록_조회_요청
+        // when
+        // 지하철_노선_목록_조회_요청
 
-    // then
-    // 지하철_노선_목록_응답됨
-    // 지하철_노선_목록_포함됨
-    @get:Test
-    @get:DisplayName("지하철 노선 목록을 조회한다.")
-    val lines: Unit
-        get() {
-            // given
-            // 지하철_노선_등록되어_있음
-            // 지하철_노선_등록되어_있음
+        // then
+        // 지하철_노선_목록_응답됨
+        // 지하철_노선_목록_포함됨
+    }
 
-            // when
-            // 지하철_노선_목록_조회_요청
+    @Test
+    @DisplayName("지하철 노선을 조회한다.")
+    fun getLine() {
+        // given
+        // 지하철_노선_등록되어_있음
+        val params: MutableMap<String, String> = HashMap()
+        params["name"] = "신분당선"
+        params["color"] = "bg-red-600"
+        params["startTime"] = LocalTime.of(5, 30).format(DateTimeFormatter.ISO_TIME)
+        params["endTime"] = LocalTime.of(23, 30).format(DateTimeFormatter.ISO_TIME)
+        params["intervalTime"] = "5"
+        val createResponse = RestAssured.given().log().all().contentType(MediaType.APPLICATION_JSON_VALUE).body(params).`when`().post("/lines").then().log().all().extract()
 
-            // then
-            // 지하철_노선_목록_응답됨
-            // 지하철_노선_목록_포함됨
-        }
+        // when
+        // 지하철_노선_조회_요청
+        val uri = createResponse.header("Location")
+        val response = RestAssured.given().log().all().accept(MediaType.APPLICATION_JSON_VALUE).`when`()[uri].then().log().all().extract()
 
-    // given
-    // 지하철_노선_등록되어_있음
-    @get:Test
-    @get:DisplayName("지하철 노선을 조회한다.")
-    val line:
-
-            // when
-            // 지하철_노선_조회_요청
-
-            // then
-            // 지하철_노선_응답됨
-            Unit
-        get() {
-            // given
-            // 지하철_노선_등록되어_있음
-            val params: MutableMap<String, String> = HashMap()
-            params["name"] = "신분당선"
-            params["color"] = "bg-red-600"
-            params["startTime"] = LocalTime.of(5, 30).format(DateTimeFormatter.ISO_TIME)
-            params["endTime"] = LocalTime.of(23, 30).format(DateTimeFormatter.ISO_TIME)
-            params["intervalTime"] = "5"
-            val createResponse = RestAssured.given().log().all().contentType(MediaType.APPLICATION_JSON_VALUE).body(params).`when`().post("/lines").then().log().all().extract()
-
-            // when
-            // 지하철_노선_조회_요청
-            val uri = createResponse.header("Location")
-            val response = RestAssured.given().log().all().accept(MediaType.APPLICATION_JSON_VALUE).`when`()[uri].then().log().all().extract()
-
-            // then
-            // 지하철_노선_응답됨
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
-            assertThat(response.`as`(LineResponse::class.java)).isNotNull()
-        }
+        // then
+        // 지하철_노선_응답됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
+        assertThat(response.`as`(LineResponse::class.java)).isNotNull()
+    }
 
     @DisplayName("지하철 노선을 수정한다.")
     @Test
