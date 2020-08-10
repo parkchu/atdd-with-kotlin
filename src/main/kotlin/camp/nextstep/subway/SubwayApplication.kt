6f011c829ffb65a2e644feddd7bbaf86a28f7dd4
javajigi <javajigi@gmail.com>
2020-08-10 17:35:24 +0900

    initial commit

---
+++ src/main/kotlin/camp/nextstep/subway/SubwayApplication.kt
@@ -0,0 +1,11 @@
+package camp.nextstep.subway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubwayApplication
+
+fun main(args: Array<String>) {
+	runApplication<SubwayApplication>(*args)
+}
