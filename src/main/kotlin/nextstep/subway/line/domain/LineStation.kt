044a0df24d4f1f39e7aeb46d2f605158fa9fa15c
javajigi <javajigi@gmail.com>
2020-08-19 09:13:32 +0900

    feat: add LineStation

---
+++ src/main/kotlin/nextstep/subway/line/domain/LineStation.kt
@@ -0,0 +1,20 @@
+package nextstep.subway.line.domain
+
+import javax.persistence.*
+
+@Entity
+@Table
+class LineStation(
+        @Column(nullable = false)
+        val stationId: Long,
+
+        var preStationId: Long? = null,
+
+        var distance: Int? = null,
+
+        var duration: Int? = null,
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long = 0
+)
