b624ace0c0f48a6d97c89a6a69815b72cdd15678
Parkjoopika <parkjoohan0603@gmail.com>
2020-09-30 16:26:06 +0900

    refactor: test LineStations can't add same LineStation

--- src/test/kotlin/nextstep/subway/line/domain/LineStationsTest.kt
+++ src/test/kotlin/nextstep/subway/line/domain/LineStationsTest.kt
@@ -1,12 +1,14 @@
 package nextstep.subway.line.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import java.lang.RuntimeException
 
 class LineStationsTest {
     @Test
     fun add_line_station() {
-        // Give
+        // Given
         val lineStation = LineStation(1, null, 0, 0)
 
         // When
@@ -15,5 +17,19 @@ class LineStationsTest {
 
         // Then
         assertThat(lineStations.lineStations).hasSize(1)
+    }
+
+    @Test
+    fun can_not_add_same_station() {
+        // Given
+        val lineStation = LineStation(1, null, 0, 0)
+        val lineStations = LineStations()
+        lineStations.add(lineStation)
+
+        // Then
+        assertThatThrownBy {
+            // When
+            lineStations.add(lineStation)
+        }.isInstanceOf(RuntimeException::class.java)
     }
 }
