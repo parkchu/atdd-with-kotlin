1d38972a82f685ba1a04c2a332c5f1431a5cb038
Parkjoopika <parkjoohan0603@gmail.com>
2020-10-18 21:24:36 +0900

    feat: lineService의 deleteStation, deleteStationAllLine 메소드를 테스트 하는 테스트 코드를 작성했다.

--- src/test/kotlin/nextstep/subway/line/domain/LineStationServiceTest.kt
+++ src/test/kotlin/nextstep/subway/line/domain/LineStationServiceTest.kt
@@ -6,6 +6,7 @@ import nextstep.subway.line.dto.LineStationRequest
 import nextstep.subway.station.domain.Station
 import nextstep.subway.station.domain.StationRepository
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
@@ -21,23 +22,57 @@ class LineStationServiceTest {
     @Mock
     private lateinit var stationRepository: StationRepository
 
-    @Test
-    fun addLineStation() {
-        // Given
-        val line = Line("테스트선", "yellow",  LocalTime.of(5, 30), LocalTime.of(23, 30), 5)
-        val station = Station("테스트역")
+    private val line = Line("테스트선", "yellow",  LocalTime.of(5, 30), LocalTime.of(23, 30), 5)
+    private val station = Station("테스트역")
+    private val lineStationRequest = LineStationRequest(station.id, null, 1, 1)
+
+    @BeforeEach
+    fun backGround() {
         `when`(lineRepository.findById(line.id)).thenReturn(Optional.of(line))
         `when`(stationRepository.findById(station.id)).thenReturn(Optional.of(station))
         `when`(stationRepository.findAll()).thenReturn(listOf(station))
+    }
+
+    @Test
+    fun addLineStation() {
+        // Given
         val lineService = LineService(lineRepository, stationRepository)
 
         // When
-        val lineStationRequest = LineStationRequest(station.id, null, 1, 1)
         lineService.addStation(line.id, lineStationRequest)
 
         // Then
         val lineStations = lineService.findLineById(line.id).stations
         assertThat(lineStations).hasSize(1)
 
+    }
+
+    @Test
+    fun deleteLineStation() {
+        // Given
+        val lineService = LineService(lineRepository, stationRepository)
+        lineService.addStation(line.id, lineStationRequest)
+
+        // When
+        lineService.deleteStation(line.id, station.id)
+
+        // Then
+        val lineStations = lineService.findLineById(line.id).stations
+        assertThat(lineStations).isEmpty()
+    }
+
+    @Test
+    fun deleteLineStationAllLine() {
+        // Given
+        val lineService = LineService(lineRepository, stationRepository)
+        lineService.addStation(line.id, lineStationRequest)
+        `when`(lineRepository.findByStationContains(station.id)).thenReturn(listOf(line).filter { line -> line.lineStations.lineStations.any { it.stationId == station.id } })
+
+        // When
+        lineService.deleteStationAllLine(station.id)
+
+        // Then
+        val lineStations = lineService.findLineById(line.id).stations
+        assertThat(lineStations).isEmpty()
     }
 }
