331c6eb62a6643ed58f6fb67fe701da2198812e9
Parkjoopika <parkjoohan0603@gmail.com>
2020-10-20 00:53:44 +0900

    refactor: 테스트 코드의 중복을 제거했다.

--- src/test/kotlin/nextstep/subway/map/acceptance/MapLookupTest.kt
+++ src/test/kotlin/nextstep/subway/map/acceptance/MapLookupTest.kt
@@ -1,104 +1,59 @@
 package nextstep.subway.map.acceptance
 
-import io.restassured.RestAssured
 import nextstep.subway.AcceptanceTest
 import nextstep.subway.line.acceptance.LineAcceptanceStep.등록한_노선정보_요청
 import nextstep.subway.line.acceptance.LineAddStationStep.노선에_역_등록되어_있음
-import nextstep.subway.map.dto.MapResponse
+import nextstep.subway.line.dto.LineResponse
+import nextstep.subway.map.acceptance.MapLookupStep.노선도_조회_실패됨
+import nextstep.subway.map.acceptance.MapLookupStep.노선도_조회_요청
+import nextstep.subway.map.acceptance.MapLookupStep.노선도_조회됨
+import nextstep.subway.map.acceptance.MapLookupStep.노선별_지하철역_순서_정렬됨
 import nextstep.subway.station.acceptance.StationAcceptanceStep.등록한_지하철역정보_요청
-import org.assertj.core.api.Assertions.assertThat
-import org.hamcrest.CoreMatchers.notNullValue
+import nextstep.subway.station.dto.StationResponse
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 
 @DisplayName("지하철 노선도 조회 기능")
 class MapLookupTest : AcceptanceTest() {
+    val stations = mutableListOf<StationResponse>()
+    val lines = mutableListOf<LineResponse>()
+
+    @BeforeEach
+    fun backGround() {
+        stations.clear()
+        repeat(5) { stations.add(등록한_지하철역정보_요청("$it")) }
+        lines.clear()
+        repeat(2) { lines.add(등록한_노선정보_요청("${it}호선", "bg-red-600", "5")) }
+        노선에_역_등록되어_있음(stations[0].id, lines[0].id)
+        노선에_역_등록되어_있음(stations[1].id, lines[0].id)
+        노선에_역_등록되어_있음(stations[2].id, lines[0].id)
+        노선에_역_등록되어_있음(stations[0].id, lines[1].id)
+        노선에_역_등록되어_있음(stations[3].id, lines[1].id)
+        노선에_역_등록되어_있음(stations[4].id, lines[1].id)
+    }
 
     @DisplayName("지하철 노선도를 조회한다.")
     @Test
     fun lookupRouteMap() {
-        // Given
-        val station1 = 등록한_지하철역정보_요청("일역")
-        val station2 = 등록한_지하철역정보_요청("이역")
-        val station3 = 등록한_지하철역정보_요청("삼역")
-        val station4 = 등록한_지하철역정보_요청("사역")
-        val station5 = 등록한_지하철역정보_요청("오역")
-        val line1 = 등록한_노선정보_요청("1호선", "bg-red-600", "5")
-        val line2 = 등록한_노선정보_요청("2호선", "bg-red-600", "5")
-        노선에_역_등록되어_있음(station1.id, line1.id)
-        노선에_역_등록되어_있음(station2.id, line1.id)
-        노선에_역_등록되어_있음(station3.id, line1.id)
-        노선에_역_등록되어_있음(station1.id, line2.id)
-        노선에_역_등록되어_있음(station4.id, line2.id)
-        노선에_역_등록되어_있음(station5.id, line2.id)
-
         // When
-        val response = RestAssured
-                .given()
-                .log().all()
-                .header("If-None-Match", "")
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .`when`()
-                .get("/maps")
-                .then()
-                .header("ETag", notNullValue())
-                .log().all().extract()
+        val response = 노선도_조회_요청()
 
         // Then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
-        val mapResponse = response.`as`(MapResponse::class.java)
-        assertThat(mapResponse.lineResponses[0].stations.first().station.id).isEqualTo(station1.id)
-        assertThat(mapResponse.lineResponses[0].stations[1].station.id).isEqualTo(station2.id)
-        assertThat(mapResponse.lineResponses[0].stations.last().station.id).isEqualTo(station3.id)
-        assertThat(mapResponse.lineResponses[1].stations.first().station.id).isEqualTo(station1.id)
-        assertThat(mapResponse.lineResponses[1].stations[1].station.id).isEqualTo(station4.id)
-        assertThat(mapResponse.lineResponses[1].stations.last().station.id).isEqualTo(station5.id)
+        노선도_조회됨(response)
+        노선별_지하철역_순서_정렬됨(response, stations)
     }
 
     @DisplayName("변하지 않은 지하철 노선도를 조회한다.")
     @Test
     fun lookupRouteMap2() {
-        // Given
-        val station1 = 등록한_지하철역정보_요청("일역")
-        val station2 = 등록한_지하철역정보_요청("이역")
-        val station3 = 등록한_지하철역정보_요청("삼역")
-        val station4 = 등록한_지하철역정보_요청("사역")
-        val station5 = 등록한_지하철역정보_요청("오역")
-        val line1 = 등록한_노선정보_요청("1호선", "bg-red-600", "5")
-        val line2 = 등록한_노선정보_요청("2호선", "bg-red-600", "5")
-        노선에_역_등록되어_있음(station1.id, line1.id)
-        노선에_역_등록되어_있음(station2.id, line1.id)
-        노선에_역_등록되어_있음(station3.id, line1.id)
-        노선에_역_등록되어_있음(station1.id, line2.id)
-        노선에_역_등록되어_있음(station4.id, line2.id)
-        노선에_역_등록되어_있음(station5.id, line2.id)
-        val response = RestAssured
-                .given()
-                .log().all()
-                .header("If-None-Match", "")
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .`when`()
-                .get("/maps")
-                .then()
-                .header("ETag", notNullValue())
-                .log().all().extract()
+        val response = 노선도_조회_요청()
 
         // When
         val eTag = response.header("ETag")
-        val response2 = RestAssured
-                .given()
-                .log().all()
-                .header("If-None-Match", eTag)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .`when`()
-                .get("/maps")
-                .then()
-                .header("ETag", notNullValue())
-                .log().all().extract()
+        val response2 = 노선도_조회_요청(eTag)
 
         // Then
-        assertThat(response2.statusCode()).isEqualTo(HttpStatus.NOT_MODIFIED.value())
+        노선도_조회_실패됨(response2)
     }
 }
