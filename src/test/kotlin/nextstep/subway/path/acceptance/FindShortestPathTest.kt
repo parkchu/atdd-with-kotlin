f312bd2a748f17a5700071401bacf84e16ca0894
Parkjoopika <parkjoohan0603@gmail.com>
2020-10-25 02:00:23 +0900

    feat: test파일을 만듬

---
+++ src/test/kotlin/nextstep/subway/path/acceptance/FindShortestPathTest.kt
@@ -0,0 +1,14 @@
+package nextstep.subway.path.acceptance
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("지하철 경로 검색")
+class FindShortestPathTest {
+
+    @DisplayName("두 역의 최단 거리 경로를 조회")
+    @Test
+    fun findShortestPath() {
+
+    }
+}
