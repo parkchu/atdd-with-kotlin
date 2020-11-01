5409d9b567c25099a9f3ed9dac70c9403c23e4b8
Parkjoopika <parkjoohan0603@gmail.com>
2020-11-01 22:06:47 +0900

    feat: Path에 같은 LineStation을 add할려고 하는경우 해당 LineStation부터 마지막의 LineStation 까지 제거하는 기능 구현

--- src/main/kotlin/nextstep/subway/path/domain/Path.kt
+++ src/main/kotlin/nextstep/subway/path/domain/Path.kt
@@ -5,23 +5,27 @@ import nextstep.subway.path.dto.PathStationResponse
 import nextstep.subway.station.domain.Station
 
 class Path(val startStationId: Long, val arrivalStationId: Long) {
-    private val _stations: MutableList<Long> = mutableListOf()
-    val stations: List<Long>
+    private val _stations: MutableList<LineStation> = mutableListOf()
+    val stations: List<LineStation>
         get() = _stations.toList()
-    var distance: Int = 0
-        private set
-    var duration: Int = 0
-        private set
+    val distance: Int
+        get() = _stations.sumBy { it.distance }
+    val duration: Int
+        get() = _stations.sumBy { it.duration }
 
     fun add(lineStation: LineStation) {
-        _stations.add(lineStation.stationId)
-        distance += lineStation.distance
-        duration += lineStation.duration
+        _stations.add(relocationStations(lineStation))
+    }
+
+    private fun relocationStations(lineStation: LineStation): LineStation {
+        val index = _stations.indexOf(_stations.find { it.stationId == lineStation.stationId } ?: return lineStation)
+        _stations.retainAll(_stations.take(index))
+        return lineStation
     }
 
     fun getStationsResponse(stations: List<Station>): List<PathStationResponse> {
-        return this.stations.map {
-            PathStationResponse.of(stations.find { station -> station.id == it } ?: throw RuntimeException())
+        return this._stations.map {
+            PathStationResponse.of(stations.find { station -> station.id == it.stationId } ?: throw RuntimeException())
         }
     }
 }
--- src/test/kotlin/nextstep/subway/path/domain/PathTest.kt
+++ src/test/kotlin/nextstep/subway/path/domain/PathTest.kt
@@ -21,12 +21,12 @@ class PathTest {
     @Test
     fun addStation() {
         val path = Path(1, 1)
-        val lineStation = LineStation(1, 1, 10, 10)
+        val lineStation = LineStation(1, 1, 5, 10)
 
         path.add(lineStation)
 
-        assertThat(path.stations.first()).isEqualTo(lineStation.stationId)
-        assertThat(path.distance).isEqualTo(10)
+        assertThat(path.stations.first()).isEqualTo(lineStation)
+        assertThat(path.distance).isEqualTo(5)
         assertThat(path.duration).isEqualTo(10)
     }
 
@@ -43,5 +43,24 @@ class PathTest {
         assertThat(pathStationResponse.first().name).isEqualTo(stations.first().name)
         assertThat(pathStationResponse.first().createdAt).isEqualTo(stations.first().createdDate)
         assertThat(pathStationResponse.first()::class.java).isEqualTo(PathStationResponse::class.java)
+    }
+
+    @Test
+    fun addSameLineStation() {
+        val path = Path(1, 1)
+        val lineStation = LineStation(1, null, 5, 10)
+        val lineStation2 = LineStation(2, 1, 5, 10)
+        val lineStation3 = LineStation(3, 2, 5, 10)
+        path.add(lineStation)
+        path.add(lineStation2)
+        path.add(lineStation3)
+        assertThat(path.stations.last()).isEqualTo(lineStation3)
+        assertThat(path.stations).hasSize(3)
+
+        path.add(lineStation2)
+
+        assertThat(path.stations.last()).isEqualTo(lineStation2)
+        assertThat(path.stations).hasSize(2)
+
     }
 }
