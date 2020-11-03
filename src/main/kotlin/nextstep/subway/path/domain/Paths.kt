ec04626159210bc7a3b4fe525e41a7bff4c08093
Parkjoopika <parkjoohan0603@gmail.com>
2020-11-03 22:31:32 +0900

    feat: Paths에서 거리를 기준으로 최단경로를 구하는 기능을 구현했다.

---
+++ src/main/kotlin/nextstep/subway/path/domain/Paths.kt
@@ -0,0 +1,13 @@
+package nextstep.subway.path.domain
+
+class Paths {
+    private val _paths = mutableListOf<Path>()
+
+    fun add(path: Path) {
+        _paths.add(path)
+    }
+
+    fun getShortestPath(): Path {
+        return _paths.minBy { it.distance } ?: throw RuntimeException("출발역과 도착역을 이어주는 경로가 하나도 존재하지 않을경우의 예외처리")
+    }
+}
